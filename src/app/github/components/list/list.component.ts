7949bf508556ba06ebee10eaa6aabcc4fd3cb8b6
Jefferson Martins <jef.martins.s@gmail.com>
2022-03-18 19:29:03 -0300

    corrigido conflito

--- src/app/github/components/list/list.component.ts
+++ src/app/github/components/list/list.component.ts
@@ -9,25 +9,32 @@ import { lastValueFrom } from 'rxjs';
 })
 export class ListComponent implements OnInit {
 
-  @Input() itens:any;
+  @Input() itens: any;
   itensAdapted: any = []
 
   constructor(public api: GithubApi) { }
 
   ngOnInit(): void {
-    this.itens?.subscribe(async (itens:any)=>{
-      if(!itens)return;
+    this.itens?.subscribe(async (itens: any) => {
+      if (!itens) return;
 
       for await (let item of itens) {
-        item.owner.login, item.name, item.commits_url
+
         const commit = item.commits_url.split("{");
-        const sha:any = await lastValueFrom(this.api.getUrl(commit[0]));
-        const url = "https://github.com/" + item.owner.login + "/" + item.name + "/archive/" + sha[0].sha + ".zip"
-        
-        this.itensAdapted.push({
-          ...item,
-          download_url_adpted: url
-        })
+
+        try{
+          const sha: any = await lastValueFrom(this.api.getUrl(commit[0]));
+
+          const url = "https://github.com/" + item.owner.login + "/" + item.name + "/archive/" + sha[0].sha + ".zip"
+
+          this.itensAdapted.push({
+            ...item,
+            download_url_adpted: url
+          });
+
+        }catch(e){
+          console.log(e);
+        }
       }
     });
   }
